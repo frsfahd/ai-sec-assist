194a9ab66612fe160afb72e87226602066727ab0
farraselfahd <farraselfahd2000@gmail.com>
2025-12-10 20:02:42 +0700

    clean unused import

--- src/main.py
+++ src/main.py
@@ -1,39 +1,46 @@
-from fileinput import filename
 import logging
 import os
+
 import dotenv
+from telegram import Update
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    ContextTypes,
+    MessageHandler,
+    filters,
+)
 
 from ai import RunAgent
-
-from telegram import Update
-from telegram.ext import filters, MessageHandler, ApplicationBuilder, ContextTypes, CommandHandler
 
 dotenv.load_dotenv()
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
 mediaList: dict[str, dict[int, dict[str, str]]] = {}
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!"
+    )
+
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     response = await RunAgent((update.message.from_user.username, update.message.text))
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(response.content))
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=str(response.content)
+    )
+
 
 async def media(update: Update, context: ContextTypes.DEFAULT_TYPE):
     doc = update.message.document
 
-
     mediaList[update.message.from_user.username] = {
-        update.message.id: {
-            "file_id": doc.file_id,
-            "filename": doc.file_name
-        }
+        update.message.id: {"file_id": doc.file_id, "filename": doc.file_name}
     }
 
     response = f"""
@@ -42,47 +49,59 @@ async def media(update: Update, context: ContextTypes.DEFAULT_TYPE):
     Type: {doc.mime_type}
     """
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=update.message.id, text=response)
-    
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        reply_to_message_id=update.message.id,
+        text=response,
+    )
+
 
 async def messageWithMedia(update: Update, context: ContextTypes.DEFAULT_TYPE):
-
     # Check if reply is to a document message
     replied_message = update.message.reply_to_message
     if not replied_message or not replied_message.document:
         return  # Skip if not replying to a document
 
     # get the file url and metadata to download from telegram server
-    thread = mediaList[update.message.from_user.username][update.message.reply_to_message.id]
+    thread = mediaList[update.message.from_user.username][
+        update.message.reply_to_message.id
+    ]
     file_id = thread["file_id"]
     filename = thread["filename"]
     file = await context.bot.get_file(file_id=file_id)
 
     # download file
-    filepath = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "tmp", filename))
+    filepath = os.path.abspath(
+        os.path.join(os.path.dirname(__file__), "..", "tmp", filename)
+    )
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
     await file.download_to_drive(filepath)
 
-    prompt = f"""filepath: {filepath}  
+    prompt = f"""filepath: {filepath}
 {update.message.text}
 """
 
     response = await RunAgent((update.message.from_user.username, prompt))
 
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(response.content))
+    await context.bot.send_message(
+        chat_id=update.effective_chat.id, text=str(response.content)
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     application = ApplicationBuilder().token(os.getenv("BOT_API_TOKEN")).build()
-    
-    start_handler = CommandHandler('start', start)
+
+    start_handler = CommandHandler("start", start)
     chatHandler = MessageHandler(filters.TEXT & (~filters.COMMAND), message)
     mediaHandler = MessageHandler(filters.Document.ALL & (~filters.COMMAND), media)
-    messageWithMediaHandler = MessageHandler(filters.TEXT & filters.REPLY &(~filters.COMMAND), messageWithMedia)
+    messageWithMediaHandler = MessageHandler(
+        filters.TEXT & filters.REPLY & (~filters.COMMAND), messageWithMedia
+    )
 
     application.add_handler(start_handler)
-    application.add_handler(messageWithMediaHandler)  
+    application.add_handler(messageWithMediaHandler)
     application.add_handler(chatHandler)
     application.add_handler(mediaHandler)
-    
+
     application.run_polling()
